c292345502ae3480fb84f3b23f9f10ac965bfe48
Vlad Belskiy <ubelski@exadel.com>
2015-05-17 12:13:23 +0300

    ngProgress, login form

--- public/teacher/app.js
+++ public/teacher/app.js
@@ -13,9 +13,22 @@ angular.module('app', [
     'ui.grid.resizeColumns',
     'ui.grid.pinning',
     'ui.grid.selection',
-    'ui.grid.moveColumns'
-
+    'ui.grid.moveColumns',
+    'ngProgress'
 ])
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.otherwise({redirectTo: '/home'});
+    }])
+    .run(['$rootScope','ngProgress', function ($rootScope, ngProgress) {
+        var changeColorProgressbar = $rootScope.$on('$locationChangeStart', function () {
+            ngProgress.color('#FFFFFF');
+            changeColorProgressbar();
+        });
+        $rootScope.$on('$locationChangeStart', function (event) {
+            ngProgress.start();
+        });
+        $rootScope.$on('$routeChangeSuccess', function () {
+            ngProgress.complete();
+        });
+
     }]);
